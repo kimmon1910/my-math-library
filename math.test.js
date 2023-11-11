c6a282db9e5274bfb7b42346d59f75155d1ef81b
Napat Boonsej <napat.za.z@gmail.com>
2023-11-11 12:16:27 +0700

    Addsum array functionality V.2 test 5

--- math.test.js
+++ math.test.js
@@ -1,5 +1,6 @@
+// test.js
 const math = require('./mymath');
 const assert = require('assert');
 
-assert.strictEqual(math.sum(3,2),5 ,'should sum two numders');
-assert.strictEqual(math.sum([1,2,3,4,5]),15 ,'should sum an array');
+assert.strictEqual(math.sum(3, 2), 5, 'should sum two numbers');
+assert.strictEqual(math.sum([1, 2, 3, 4, 5]), 15, 'should sum an array');
--- mymath.js
+++ mymath.js
@@ -1,7 +1,10 @@
+// mymath.js
 function sum(a, b) {
-  if(Array.isArray(a)){
-    return a.reduce((acc, n) => acc + n, 0)
-      }    else if (typeof a === 'number' && typeof b === 'number'){
-        return a+b;
-    }
+  if (Array.isArray(a)) {
+    return a.reduce((acc, n) => acc + n, 0);
+  } else if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
   }
+}
+
+module.exports = sum;
