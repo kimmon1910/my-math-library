2039e3866cb10e461b7fdb33ff2356526e547e07
Napat Boonsej <napat.za.z@gmail.com>
2023-11-11 11:25:34 +0700

    Addsum array functionality V.2 tes32

--- mymath.js
+++ mymath.js
@@ -1,8 +1,7 @@
 function sum(a, b) {
   if(Array.isArray(a)){
-    return a.reduce((acc, n) => acc + n, 0);
-    }
-    else if (typeof a === 'number' && typeof b === 'number'){
+    return a.reduce((acc, n) => acc + n, 0)
+      }    else if (typeof a === 'number' && typeof b === 'number'){
         return a+b;
     }
   }
