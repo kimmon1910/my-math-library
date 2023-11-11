9120b945fc3b37efaf095839bbde13670da20672
Napat Boonsej <napat.za.z@gmail.com>
2023-11-11 10:52:24 +0700

    Addsum array functionality NWE

--- math.test.js
+++ math.test.js
@@ -1,5 +1,5 @@
-const math = requier('./math');
-const assert = requier('assert');
+const math = Requier('./math');
+const assert = Requier('assert');
 
 assert.strictEqual(math.sum(3,2),5 ,'should sum two numders');
 assert.strictEqual(math.sum([1,2,3,4,5]),15 ,'should sum an array');
